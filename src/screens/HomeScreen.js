8d56ab1cc24c293ff63c7decacb25bb2c016313f
RON J SCOTT <70779328+RONSHX@users.noreply.github.com>
2025-09-21 10:06:20 +0530

    Update HomeScreen.js

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -729,12 +729,20 @@ export default function HomeScreen() {
         </View>
 
         <View testID="categoryList">
-       
+          <Categories
+            categories={categories}
+            activeCategory={activeCategory}
+            handleChangeCategory={handleChangeCategory}
+          />
         </View>
 
         <View testID="foodList">
+          <FoodItems 
+            foods={filteredfoods} 
+            categories={categories} 
+          />
+        </View>
 
-          </View>
       </ScrollView>
     </View>
   );
